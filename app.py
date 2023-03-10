0efc8c868160dcec12a98ba381aa9cebe77a0c93
Vaibhav dwivedi <vickydwivedi989@gmail.com>
2023-03-10 15:09:49 +0530

    replaced sqlite wih postgres

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_smorest import Api
-from flask_jwt_extended import JWTManager
-from blocklist import BLOCKLIST
 from resources.projects import blp as projectblueprint
 from resources.managers import blp as managerblueprint
 from resources.users import blp as userblueprint
-
-from db import db
+from db import db 
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +11,7 @@ def create_app():
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://xrxhxzxo:4ki8lS1a1VOkCBj010-PDBucRvEtk8_z@mahmud.db.elephantsql.com/xrxhxzxo"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
@@ -22,68 +19,9 @@ def create_app():
     app.config["JWT_SECRET_KEY"] = "vicky"
 
     app.secret_key="my secret key"
-    
 
     db.init_app(app)
     api = Api(app)
-    jwt = JWTManager(app)
-    
-    
-    @jwt.token_in_blocklist_loader
-    def check_if_token_in_blocklist(jwt_header, jwt_payload):
-        return jwt_payload["jti"] in BLOCKLIST
-    
-    
-    @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return (
-            jsonify({"message": "The token has expired.", "error": "token_expired"}),
-            401,
-        )
-    
-    @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return (
-            jsonify(
-                {"message": "Signature verification failed.", "error": "invalid_token"}
-            ),
-            401,
-        )
-    
-    @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return (
-            jsonify(
-                {
-                    "description": "Request does not contain an access token.",
-                    "error": "authorization_required",
-                }
-            ),
-            401,
-        )
-    
-    @jwt.needs_fresh_token_loader
-    def token_not_fresh_callback(jwt_header, jwt_payload):
-        return (
-            jsonify(
-                {
-                    "description": "The token is not fresh.",
-                    "error": "fresh_token_required",
-                }
-            ),
-            401,
-        )
-    
-    @jwt.revoked_token_loader
-    def revoked_token_callback(jwt_header, jwt_payload):
-        return (
-            jsonify(
-                {"description": "The token has been revoked.", "error": "token_revoked"}
-            ),
-            401,
-        )
-
-
 
     with app.app_context():
         db.create_all()
@@ -92,8 +30,6 @@ def create_app():
     api.register_blueprint(managerblueprint)
     api.register_blueprint(userblueprint)
 
-
-    
     return app    
 
 
